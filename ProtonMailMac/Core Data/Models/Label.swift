9f4524320ee7040cb76fc88e9c1797c632c6a8ce
Marcel Piešťanský <marcel@piestansky.cz>
2021-09-05 21:04:56 +0200

    Added "defaultOrder" property

--- ProtonMailMac/Core Data/Models/Label.swift
+++ ProtonMailMac/Core Data/Models/Label.swift
@@ -83,4 +83,28 @@ extension Label {
             return self.labelID == "1"
         }
     }
+    
+    var defaultOrder: Int {
+        switch self.labelID {
+        case "0":
+            return 1
+        case "1", "8":
+            return 2
+        case "2", "7":
+            return 3
+        case "3":
+            return 7
+        case "4":
+            return 6
+        case "5":
+            return 8
+        case "6":
+            return 5
+        case "10":
+            return 4
+        default:
+            fatalError("Unexpected application state.")
+        }
+    }
+    
 }
