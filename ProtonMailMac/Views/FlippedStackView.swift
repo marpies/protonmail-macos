30436655040b49008a995a6406956288eeb086fb
Marcel Piešťanský <marcel@piestansky.cz>
2021-09-20 15:55:45 +0200

    Added FlippedStackView

---
+++ ProtonMailMac/Views/FlippedStackView.swift
@@ -0,0 +1,17 @@
+//
+//  FlippedStackView.swift
+//  ProtonMailMac
+//
+//  Created by Marcel Piešťanský on 17.09.2021.
+//  Copyright © 2021 marpies. All rights reserved.
+//
+
+import Cocoa
+
+class FlippedStackView: NSStackView {
+
+    override var isFlipped: Bool {
+        return true
+    }
+    
+}
