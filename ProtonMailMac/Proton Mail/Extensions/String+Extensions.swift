e762163a13c0988289dac78019c64f53331486d6
Marcel Piešťanský <marcel@piestansky.cz>
2021-09-25 18:33:44 +0200

    Added additional String extensions

--- ProtonMailMac/Proton Mail/Extensions/String+Extensions.swift
+++ ProtonMailMac/Proton Mail/Extensions/String+Extensions.swift
@@ -82,7 +82,7 @@ extension String {
                 return replacedString
             }
         } catch let ex as NSError {
-            //            PMLog.D("\(ex)")
+            PMLog.D("\(ex)")
         }
         return self
     }
@@ -95,7 +95,7 @@ extension String {
                                     options: NSRegularExpression.MatchingOptions(rawValue: 0),
                                     range: NSRange(location: 0, length: self.count)) != nil
         } catch let ex as NSError {
-            //            PMLog.D("\(ex)")
+            PMLog.D("\(ex)")
         }
         
         return false
@@ -127,7 +127,7 @@ extension String {
         var firstError : Error?
         for key in keys {
             do {
-                if let token = key.token, let signature = key.signature { //have both means new schema. key is
+                if let token = key.token, let _ = key.signature { //have both means new schema. key is
                     if let plaitToken = try token.decryptMessage(binKeys: userKeys, passphrase: passphrase) {
                         //PMLog.D(signature)
                         return try Crypto().decryptVerify(encrytped: self,
@@ -240,4 +240,201 @@ extension String {
         return nil
     }
 
+}
+
+
+extension String {
+    
+    /// A string with the special characters in it escaped.
+    /// Used when passing a string into JavaScript, so the string is not completed too soon
+    /// Performance is not good for large string - Notes from Feng
+    var escaped: String {
+        var arr = [String]()
+        for u in self.utf16 {
+            arr.append("\\u\(String(format: "%04X", u))")
+        }
+        let str = arr.joined()
+        return str
+    }
+    
+    func contains(check s: String) -> Bool {
+        return self.range(of: s, options: NSString.CompareOptions.caseInsensitive) != nil ? true : false
+    }
+    
+}
+
+
+extension String {
+    
+    func parseObject() -> [String:String] {
+        if self.isEmpty {
+            return ["" : ""];
+        }
+        do {
+            let data : Data! = self.data(using: String.Encoding.utf8)
+            let decoded = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? [String:String] ?? ["" : ""]
+            return decoded
+        } catch let ex as NSError {
+            PMLog.D("\(ex)")
+        }
+        return ["":""]
+    }
+    
+    func splitByComma() -> [String] {
+        return self.components(separatedBy: ",")
+    }
+    
+    func ln2br() -> String {
+        let out = self.replacingOccurrences(of: "\r\n", with: "<br />")
+        return out.replacingOccurrences(of: "\n", with: "<br />")
+    }
+    
+    func rmln() -> String {
+        return  self.replacingOccurrences(of: "\n", with: "")
+    }
+    
+    func lr2lrln() -> String {
+        return  self.replacingOccurrences(of: "\r", with: "\r\n")
+    }
+    
+    func decodeHtml() -> String {
+        var result = self.replacingOccurrences(of: "&amp;", with: "&", options: NSString.CompareOptions.caseInsensitive, range: nil)
+        result = result.replacingOccurrences(of: "&quot;", with: "\"", options: NSString.CompareOptions.caseInsensitive, range: nil)
+        result = result.replacingOccurrences(of: "&#039;", with: "'", options: NSString.CompareOptions.caseInsensitive, range: nil)
+        result = result.replacingOccurrences(of: "&#39;", with: "'", options: NSString.CompareOptions.caseInsensitive, range: nil)
+        result = result.replacingOccurrences(of: "&lt;", with: "<", options: NSString.CompareOptions.caseInsensitive, range: nil)
+        result = result.replacingOccurrences(of: "&gt;", with: ">", options: NSString.CompareOptions.caseInsensitive, range: nil)
+        return result
+    }
+    
+    func encodeHtml() -> String {
+        var result = self.replacingOccurrences(of: "&", with: "&amp;", options: .caseInsensitive, range: nil)
+        result = result.replacingOccurrences(of: "\"", with: "&quot;", options: .caseInsensitive, range: nil)
+        result = result.replacingOccurrences(of: "'", with: "&#039;", options: .caseInsensitive, range: nil)
+        result = result.replacingOccurrences(of: "<br />", with: "\r\n", options: .caseInsensitive, range: nil)
+        result = result.replacingOccurrences(of: "<br/>", with: "\r\n", options: .caseInsensitive, range: nil)
+        result = result.replacingOccurrences(of: "<br>", with: "\r\n", options: .caseInsensitive, range: nil)
+        result = result.replacingOccurrences(of: "<", with: "&lt;", options: .caseInsensitive, range: nil)
+        result = result.replacingOccurrences(of: ">", with: "&gt;", options: .caseInsensitive, range: nil)
+        result = result.replacingOccurrences(of: "\r\n", with: "<br />", options: .caseInsensitive, range: nil)
+        return result
+    }
+    
+    func stringBySetupInlineImage(_ from : String, to: String) -> String {
+        return self.preg_replace_none_regex(from, replaceto:to)
+    }
+    
+    func multipartGetHtmlContent() -> String {
+        
+        let textplainType = "text/plain".data(using: String.Encoding.utf8)
+        let htmlType = "text/html".data(using: String.Encoding.utf8)
+        
+        guard
+            var data = self.data(using: String.Encoding.utf8) as NSData?,
+            var len = data.length as Int?
+        else {
+            return self.ln2br()
+        }
+        
+        //get boundary=
+        let boundarLine = "boundary=".data(using: String.Encoding.ascii)!
+        let boundaryRange = data.range(of: boundarLine, options: NSData.SearchOptions.init(rawValue: 0), in: NSMakeRange(0, len))
+        if boundaryRange.location == NSNotFound {
+            return self.ln2br()
+        }
+        
+        //new len
+        len = len - (boundaryRange.location + boundaryRange.length);
+        data = data.subdata(with: NSMakeRange(boundaryRange.location + boundaryRange.length, len)) as NSData
+        let lineEnd = "\n".data(using: String.Encoding.ascii)!;
+        let nextLine = data.range(of: lineEnd, options: NSData.SearchOptions.init(rawValue: 0), in: NSMakeRange(0, len))
+        if nextLine.location == NSNotFound {
+            return self.ln2br()
+        }
+        let boundary = data.subdata(with: NSMakeRange(0, nextLine.location))
+        var boundaryString = NSString(data: boundary, encoding: String.Encoding.utf8.rawValue)!
+        boundaryString = boundaryString.replacingOccurrences(of: "\"", with: "") as NSString
+        boundaryString = boundaryString.replacingOccurrences(of: "\r", with: "") as NSString
+        boundaryString = "--".appending(boundaryString as String) as NSString //+ boundaryString;
+        
+        len = len - (nextLine.location + nextLine.length);
+        data = data.subdata(with: NSMakeRange(nextLine.location + nextLine.length, len)) as NSData
+        
+        var html : String = "";
+        var plaintext : String = "";
+        
+        var count = 0;
+        let nextBoundaryLine = boundaryString.data(using: String.Encoding.ascii.rawValue)!
+        var firstboundaryRange = data.range(of: nextBoundaryLine, options: NSData.SearchOptions(rawValue: 0), in: NSMakeRange(0, len))
+        
+        if firstboundaryRange.location == NSNotFound {
+            return self.ln2br()
+        }
+        
+        while true {
+            if count >= 10 {
+                break;
+            }
+            count += 1;
+            len = len - (firstboundaryRange.location + firstboundaryRange.length) - 1;
+            data = data.subdata(with: NSMakeRange(1 + firstboundaryRange.location + firstboundaryRange.length, len)) as NSData
+            
+            if (data.subdata(with: NSMakeRange(0 , 1)) as NSData).isEqual(to: "-".data(using: String.Encoding.ascii)!) {
+                break;
+            }
+            
+            var bodyString = NSString(data: data as Data, encoding: String.Encoding.utf8.rawValue)
+            
+            let ContentEnd = data.range(of: lineEnd, options: NSData.SearchOptions(rawValue: 0), in: NSMakeRange(2, len - 2))
+            if ContentEnd.location == NSNotFound {
+                break
+            }
+            let contentType = data.subdata(with: NSMakeRange(0, ContentEnd.location)) as NSData
+            len = len - (ContentEnd.location + ContentEnd.length);
+            data = data.subdata(with: NSMakeRange(ContentEnd.location + ContentEnd.length, len)) as NSData
+            
+            bodyString = NSString(data: contentType as Data, encoding: String.Encoding.utf8.rawValue)!
+            
+            let EncodingEnd = data.range(of: lineEnd, options: NSData.SearchOptions(rawValue: 0), in: NSMakeRange(2, len - 2))
+            if EncodingEnd.location == NSNotFound {
+                break
+            }
+            let EncodingType = data.subdata(with: NSMakeRange(0, EncodingEnd.location))
+            len = len - (EncodingEnd.location + EncodingEnd.length);
+            data = data.subdata(with: NSMakeRange(EncodingEnd.location + EncodingEnd.length, len)) as NSData
+            
+            bodyString = NSString(data: EncodingType, encoding: String.Encoding.utf8.rawValue)!
+            
+            let secondboundaryRange = data.range(of: nextBoundaryLine, options: NSData.SearchOptions(rawValue: 0), in: NSMakeRange(0, len))
+            if secondboundaryRange.location == NSNotFound {
+                break
+            }
+            //get data
+            let text = data.subdata(with: NSMakeRange(1, secondboundaryRange.location - 1))
+            
+            let plainFound = contentType.range(of: textplainType!, options: NSData.SearchOptions(rawValue: 0), in: NSMakeRange(0, contentType.length))
+            if plainFound.location != NSNotFound {
+                plaintext = NSString(data: text, encoding: String.Encoding.utf8.rawValue)! as String
+            }
+            
+            let htmlFound = contentType.range(of: htmlType!, options: NSData.SearchOptions(rawValue: 0), in: NSMakeRange(0, contentType.length))
+            if htmlFound.location != NSNotFound {
+                html = NSString(data: text, encoding: String.Encoding.utf8.rawValue)! as String
+            }
+            
+            // check html or plain text
+            bodyString = NSString(data: text, encoding: String.Encoding.utf8.rawValue)!
+            
+            firstboundaryRange = secondboundaryRange
+            
+            PMLog.D(nstring: bodyString!)
+        }
+        
+        if ( html.isEmpty && plaintext.isEmpty ) {
+            return "<div><pre>" + self.rmln() + "</pre></div>"
+        }
+        
+        return html.isEmpty ? plaintext.ln2br() : html
+    }
+    
 }
