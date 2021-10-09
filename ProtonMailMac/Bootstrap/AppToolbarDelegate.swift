942ee940395bb365bf20058800fc633f7481c513
Marcel Piešťanský <marcel@piestansky.cz>
2021-10-09 14:47:48 +0200

    Moved toolbar delegate to separate file

---
+++ ProtonMailMac/Bootstrap/AppToolbarDelegate.swift
@@ -0,0 +1,63 @@
+//
+//  AppToolbarDelegate.swift
+//  ProtonMailMac
+//
+//  Created by Marcel Piešťanský on 09.10.2021.
+//  Copyright © 2021 marpies. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class AppToolbarDelegate: NSObject, NSToolbarDelegate, ToolbarUtilizingDelegate {
+    
+    private var toolbarItems: [NSToolbarItem]?
+    private var itemIdentifiers: [NSToolbarItem.Identifier]?
+    
+    let toolbar: NSToolbar
+    
+    init(toolbar: NSToolbar) {
+        self.toolbar = toolbar
+        
+        super.init()
+        
+        self.toolbar.delegate = self
+    }
+    
+    //
+    // MARK: - Toolbar utilizing delegate
+    //
+    
+    func toolbarItemsDidUpdate(identifiers: [NSToolbarItem.Identifier], items: [NSToolbarItem]) {
+        // Remove existing items
+        while !self.toolbar.items.isEmpty {
+            let index: Int = self.toolbar.items.count - 1
+            self.toolbar.removeItem(at: index)
+        }
+        
+        self.toolbarItems = items
+        self.itemIdentifiers = identifiers
+        
+        // Set new items
+        for (index, identifier) in identifiers.enumerated() {
+            self.toolbar.insertItem(withItemIdentifier: identifier, at: index)
+        }
+    }
+    
+    //
+    // MARK: - Toolbar delegate
+    //
+    
+    func toolbarDefaultItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
+        return self.itemIdentifiers ?? []
+    }
+    
+    func toolbarAllowedItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
+        return self.itemIdentifiers ?? []
+    }
+    
+    func toolbar(_ toolbar: NSToolbar, itemForItemIdentifier itemIdentifier: NSToolbarItem.Identifier, willBeInsertedIntoToolbar flag: Bool) -> NSToolbarItem? {
+        return self.toolbarItems?.first(where: { $0.itemIdentifier == itemIdentifier })
+    }
+    
+}
--- ProtonMailMac/Bootstrap/MainWindowController.swift
+++ ProtonMailMac/Bootstrap/MainWindowController.swift
@@ -16,7 +16,7 @@ class MainWindowController: NSWindowController {
     
     private let toolbar = NSToolbar(identifier: "main")
     
-    private lazy var toolbarDelegate = ToolbarDelegate(toolbar: self.toolbar)
+    private lazy var toolbarDelegate = AppToolbarDelegate(toolbar: self.toolbar)
     
     convenience init(resolver: Resolver) {
         self.init(windowNibName: "")
@@ -45,53 +45,5 @@ class MainWindowController: NSWindowController {
             self.window?.toolbarStyle = .unified
         }
     }
-    
-}
-
-
-class ToolbarDelegate: NSObject, NSToolbarDelegate, ToolbarUtilizingDelegate {
-    
-    private var toolbarItems: [NSToolbarItem]?
-    private var itemIdentifiers: [NSToolbarItem.Identifier]?
-    
-    let toolbar: NSToolbar
-    
-    init(toolbar: NSToolbar) {
-        self.toolbar = toolbar
-        
-        super.init()
-        
-        self.toolbar.delegate = self
-    }
-    
-    func toolbarItemsDidUpdate(identifiers: [NSToolbarItem.Identifier], items: [NSToolbarItem]) {
-        // Remove existing items
-        while !self.toolbar.items.isEmpty {
-            let index: Int = self.toolbar.items.count - 1
-            self.toolbar.removeItem(at: index)
-        }
-        
-        self.toolbarItems = items
-        self.itemIdentifiers = identifiers
-        
-        // Set new items
-        for (index, identifier) in identifiers.enumerated() {
-            self.toolbar.insertItem(withItemIdentifier: identifier, at: index)
-        }
-    }
-    
-    func toolbarDefaultItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
-        return self.itemIdentifiers ?? []
-    }
-    
-    func toolbarAllowedItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
-        return self.itemIdentifiers ?? []
-    }
-    
-    func toolbar(_ toolbar: NSToolbar, itemForItemIdentifier itemIdentifier: NSToolbarItem.Identifier, willBeInsertedIntoToolbar flag: Bool) -> NSToolbarItem? {
-        return self.toolbarItems?.first(where: { $0.itemIdentifier == itemIdentifier })
-    }
-    
-    
     
 }
