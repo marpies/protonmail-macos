689fd61578c681d08146d93d9b83d4f3bed03a46
Marcel Piešťanský <marcel@piestansky.cz>
2021-10-09 16:04:10 +0200

    Update title/subtitle when the conversation count changes

--- ProtonMailMac/Mailbox/MailboxWorker.swift
+++ ProtonMailMac/Mailbox/MailboxWorker.swift
@@ -18,12 +18,17 @@ class MailboxWorker: LabelToSidebarItemParsing {
     
     private let resolver: Resolver
     private let usersManager: UsersManager
+    
+    private var currentLabel: String?
+    private var itemsBadgeObserver: NSObjectProtocol?
 
 	weak var delegate: MailboxWorkerDelegate?
     
     init(resolver: Resolver) {
         self.resolver = resolver
         self.usersManager = resolver.resolve(UsersManager.self)!
+        
+        self.addObservers()
     }
 
 	func loadData(request: Mailbox.Init.Request) {
@@ -33,6 +38,8 @@ class MailboxWorker: LabelToSidebarItemParsing {
     func loadTitle(request: Mailbox.LoadTitle.Request) {
         guard let label = self.getLabel(forId: request.labelId),
               let userId = self.usersManager.activeUser?.userId else { return }
+        
+        self.currentLabel = request.labelId
         
         let item: MailboxSidebar.Item = self.getSidebarItemKind(response: label)
         
@@ -51,6 +58,30 @@ class MailboxWorker: LabelToSidebarItemParsing {
         let db: LabelsDatabaseManaging = self.resolver.resolve(LabelsDatabaseManaging.self)!
         
         return db.getLabel(byId: id)
+    }
+    
+    private func addObservers() {
+        self.itemsBadgeObserver = NotificationCenter.default.addObserver(forType: Mailbox.Notifications.ConversationCountsUpdate.self, object: nil, queue: .main, using: { [weak self] notification in
+            guard let weakSelf = self, let notification = notification,
+                  let userId = weakSelf.usersManager.activeUser?.userId,
+                  userId == notification.userId,
+                  let currentLabel = weakSelf.currentLabel else { return }
+            
+            var count: Int = -1
+            
+            for pair in notification.total {
+                guard pair.key == currentLabel else { continue }
+                
+                count = pair.value
+            }
+            
+            guard count >= 0, let label = weakSelf.getLabel(forId: currentLabel) else { return }
+            
+            let item: MailboxSidebar.Item = weakSelf.getSidebarItemKind(response: label)
+            
+            let response = Mailbox.LoadTitle.Response(item: item, numItems: count)
+            weakSelf.delegate?.mailboxTitleDidLoad(response: response)
+        })
     }
 
 }
