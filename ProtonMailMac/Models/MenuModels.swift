48c8de7165be6cee9e71358022bb97fc36ae1f75
Marcel Piešťanský <marcel@piestansky.cz>
2021-10-29 11:11:54 +0200

    Added MenuItemParsing protocol

--- ProtonMailMac/Models/MenuModels.swift
+++ ProtonMailMac/Models/MenuModels.swift
@@ -20,3 +20,53 @@ enum MenuItem {
     case item(id: MenuItemIdentifier, title: String, color: NSColor?, state: NSControl.StateValue?, icon: String?, children: [MenuItem]?, isEnabled: Bool)
     
 }
+
+protocol MenuItemParsing {
+    func getMenuItem(model: MenuItem) -> NSMenuItem
+    func getMenuItem(model: MenuItem, target: AnyObject?, selector: Selector?) -> NSMenuItem
+}
+
+extension MenuItemParsing {
+    
+    func getMenuItem(model: MenuItem) -> NSMenuItem {
+        return self.getMenuItem(model: model, target: nil, selector: nil)
+    }
+    
+    func getMenuItem(model: MenuItem, target: AnyObject?, selector: Selector?) -> NSMenuItem {
+        switch model {
+        case .separator:
+            return NSMenuItem.separator()
+        case .item(let id, let title, let color, let state, let icon, let children, let isEnabled):
+            let item: IdentifiedNSMenuItem = IdentifiedNSMenuItem()
+            item.title = title
+            item.itemId = id
+            item.isEnabled = isEnabled
+            
+            if let state = state {
+                item.state = state
+            }
+            
+            if let icon = icon {
+                if #available(macOS 11.0, *) {
+                    item.image = NSImage(systemSymbolName: icon, accessibilityDescription: nil)?.tinted(color: color)
+                } else {
+                    // todo fallback icon
+                }
+            }
+            
+            if let children = children {
+                item.submenu = NSMenu().with { menu in
+                    menu.items = children.map { self.getMenuItem(model: $0, target: target, selector: selector) }
+                }
+            }
+            
+            if isEnabled, let target = target, let selector = selector {
+                item.target = target
+                item.action = selector
+            }
+            
+            return item
+        }
+    }
+    
+}
